02543bdd00950b34f67c7743bdff5bd7fc51f916
Lydess <lightbekon@gmail.com>
2022-08-15 21:18:06 +1000

    Initial Commit

---
+++ RelaxApp/RelaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxAppApp.swift
+//  RelaxApp
+//
+//  Created by william Vise on 15/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct RelaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
